3ca3bad26ff25b2d99e1d0f14d30789cf64f13d7
kondoumh <kondoumh@gmail.com>
2023-01-22 13:15:29 +0900

    tweak

--- hello-kafka-streams/src/main/java/com/kondoumh/hellokafkastreams/streams/WordCountProcessor.java
+++ hello-kafka-streams/src/main/java/com/kondoumh/hellokafkastreams/streams/WordCountProcessor.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Grouped;
