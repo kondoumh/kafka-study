10f717adc0470a2cda2b463bd66a171bcf5d0895
kondoumh <kondoumh@gmail.com>
2023-01-23 09:42:34 +0900

    add kafka stream binder project

---
+++ hello-kafka-streams-binder/src/test/java/com/kondoumh/hellokafkastreamsbinder/HelloKafkaStreamsBinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kondoumh.hellokafkastreamsbinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloKafkaStreamsBinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
